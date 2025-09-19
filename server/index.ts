10b9bced7de067538a700b7ebf5b32cf61a02c1c
saifiimuhammad <muhammadsaifarain786@gmail.com>
2025-09-19 19:52:32 +0500

    Fixed the post  issue

--- server/index.ts
+++ server/index.ts
@@ -68,7 +68,7 @@ app.use((req, res, next) => {
   // }, () => {
   //   log(`serving on port ${port}`);
   // });
-  server.listen(5000, "127.0.0.1", () => {
-    console.log("Server running on http://127.0.0.1:5000");
+  server.listen(port, "0.0.0.0", () => {
+    console.log("Server running on http://0.0.0.0:5000");
   });
 })();
